792e35d32c4b031663f9619794f41792473f5747
rainatam <rainatam9784@gmail.com>
2018-04-26 12:34:04 +0800

    git fixed

--- Rustable/2-fs/fat32/src/vfat/dir.rs
+++ Rustable/2-fs/fat32/src/vfat/dir.rs
@@ -11,44 +11,44 @@ use vfat::{Metadata, Attributes, Timestamp, Time, Date};
 #[derive(Debug)]
 pub struct Dir {
     // FIXME: Fill me in.
-    pub first_cluster: Cluster,
-    pub vfat: Shared<VFat>,
-    pub meta: Metadata,
-    pub short_file_name: String,
-    pub lfn: String,
+    // pub first_cluster: Cluster,
+    // pub vfat: Shared<VFat>,
+    // pub meta: Metadata,
+    // pub short_file_name: String,
+    // pub lfn: String,
 }
 
 #[repr(C, packed)]
 #[derive(Copy, Clone)]
 pub struct VFatRegularDirEntry {
     // FIXME: Fill me in.
-    file_name: [ u8; 8 ],
-    file_extension: [ u8; 3 ],
-    pub meta: Metadata,
-    size_file: u32,
+    // file_name: [ u8; 8 ],
+    // file_extension: [ u8; 3 ],
+    // pub meta: Metadata,
+    // size_file: u32,
 }
 
 #[repr(C, packed)]
 #[derive(Copy, Clone)]
 pub struct VFatLfnDirEntry {
     // FIXME: Fill me in.
-    sequence_num: u8,
-    name_chars_0: [ u8; 10 ],
-    attrib: Attributes,
-    entry_type: u8,
-    checksum_dos_file_name: u8,
-    name_chars_1: [ u8; 12 ],
-    signature: u16,
-    name_chars_2: [ u8; 4 ],
+    // sequence_num: u8,
+    // name_chars_0: [ u8; 10 ],
+    // attrib: Attributes,
+    // entry_type: u8,
+    // checksum_dos_file_name: u8,
+    // name_chars_1: [ u8; 12 ],
+    // signature: u16,
+    // name_chars_2: [ u8; 4 ],
 }
 
 #[repr(C, packed)]
 #[derive(Copy, Clone)]
 pub struct VFatUnknownDirEntry {
     // FIXME: Fill me in.
-    _data_0: [ u8; 11 ],
-    attrib: Attributes,
-    _data_1: [ u8; 20 ],
+    // _data_0: [ u8; 11 ],
+    // attrib: Attributes,
+    // _data_1: [ u8; 20 ],
 }
 
 pub union VFatDirEntry {
@@ -57,20 +57,20 @@ pub union VFatDirEntry {
     long_filename: VFatLfnDirEntry,
 }
 
-impl Dir {
-    /// Finds the entry named `name` in `self` and returns it. Comparison is
-    /// case-insensitive.
-    ///
-    /// # Errors
-    ///
-    /// If no entry with name `name` exists in `self`, an error of `NotFound` is
-    /// returned.
-    ///
-    /// If `name` contains invalid UTF-8 characters, an error of `InvalidInput`
-    /// is returned.
-    pub fn find<P: AsRef<OsStr>>(&self, name: P) -> io::Result<Entry> {
+// impl Dir {
+//     /// Finds the entry named `name` in `self` and returns it. Comparison is
+//     /// case-insensitive.
+//     ///
+//     /// # Errors
+//     ///
+//     /// If no entry with name `name` exists in `self`, an error of `NotFound` is
+//     /// returned.
+//     ///
+//     /// If `name` contains invalid UTF-8 characters, an error of `InvalidInput`
+//     /// is returned.
+//     // pub fn find<P: AsRef<OsStr>>(&self, name: P) -> io::Result<Entry> {
         
-    }
-}
+//     // }
+// }
 
 // FIXME: Implement `trait::Dir` for `Dir`.
